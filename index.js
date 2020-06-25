55bda779838485652a1b829a573308ed779d33ec
hdshim <hdprince1@naver.com>
2020-06-25 16:51:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like hdshim!');
+    res.send('I like hdshim1!');
 });
 
 app.get('/start', function(req, res){
